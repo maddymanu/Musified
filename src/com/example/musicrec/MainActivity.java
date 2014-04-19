00e860b9203e0c3323ee7ff484e66a266949c8e8
Aditya Bansal <aditya.bansal93@gmail.com>
2014-04-19 16:06:47 -0700

    Init Commit - Parse User Creation Works

---
+++ src/com/example/musicrec/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.musicrec;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+
+
+public class MainActivity extends Activity {
+	
+	public static final String SERVICECMD = "com.android.music.musicservicecommand";
+	public static final String CMDNAME = "command";
+	public static final String CMDTOGGLEPAUSE = "togglepause";
+	public static final String CMDSTOP = "stop";
+	public static final String CMDPAUSE = "pause";
+	public static final String CMDPREVIOUS = "previous";
+	public static final String CMDNEXT = "next";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Parse.initialize(this, "gyy3EnWqM4shEJQTBDvz01HHKERCmt6ldNZFei9H", "j8H1tYNTndi5SdmMmxbRBUyaKZ8X3kJmvLWQvAIc");
+		
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if(currentUser == null){
+			Intent intent = new Intent(this, LoginActivity.class);
+			startActivity(intent);
+			finish();
+		} else {
+//			Intent intent = new Intent(MainActivity.this, TabsActivity.class);
+//			startActivity(intent);
+		}
+		
+		
+		IntentFilter iF = new IntentFilter();
+		iF.addAction("com.android.music.metachanged");
+		iF.addAction("com.android.music.playstatechanged");
+		iF.addAction("com.android.music.playbackcomplete");
+		iF.addAction("com.android.music.queuechanged"); 
+		registerReceiver(mReceiver, iF);
+		
+	}
+
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() { @Override
+			public void onReceive(Context context, Intent intent)
+			{
+			String action = intent.getAction();
+			String cmd = intent.getStringExtra("command");
+			Log.d("mIntentReceiver.onReceive", action + "/" + cmd);
+			String artist = intent.getStringExtra("artist");
+			String album = intent.getStringExtra("album");
+			String track = intent.getStringExtra("track");
+			Log.d("Music",artist+":"+album+":"+track);
+			}
+		};
+
+
+}
+
---
+++ src/com/example/musicrec/Welcome.java
@@ -0,0 +1,19 @@
+package com.example.musicrec;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Welcome extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+
+
+	}
+}
