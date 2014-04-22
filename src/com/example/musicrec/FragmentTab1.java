b5577bf3c77fc7064f66ef260e19d8c866b29222
Aditya Bansal <aditya.bansal93@gmail.com>
2014-04-21 19:03:21 -0700

    Got Thread-Like to work (No threads)
    Basically, its overwriting the listforeachuser

--- src/com/example/musicrec/FragmentTab1.java
+++ src/com/example/musicrec/FragmentTab1.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
 import com.facebook.Response;
 import com.facebook.model.GraphUser;
 import com.parse.FindCallback;
@@ -28,187 +29,174 @@ import com.parse.ParseUser;
 
 public class FragmentTab1 extends SherlockFragment {
 
-	public static final String SERVICECMD = "com.android.music.musicservicecommand";
-	public static final String CMDNAME = "command";
-	public static final String CMDTOGGLEPAUSE = "togglepause";
-	public static final String CMDSTOP = "stop";
-	public static final String CMDPAUSE = "pause";
-	public static final String CMDPREVIOUS = "previous";
-	public static final String CMDNEXT = "next";
+  public static final String SERVICECMD = "com.android.music.musicservicecommand";
+  public static final String CMDNAME = "command";
+  public static final String CMDTOGGLEPAUSE = "togglepause";
+  public static final String CMDSTOP = "stop";
+  public static final String CMDPAUSE = "pause";
+  public static final String CMDPREVIOUS = "previous";
+  public static final String CMDNEXT = "next";
 
-	Song currSong;
-	private List<Song> songArrayList = null;
-	int i=0;
+  Song currSong;
+  private List<Song> songArrayList = null;
+  int i = 0;
+  Thread a, b, c;
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.tab_feed, container, false);
+  @SuppressWarnings("deprecation")
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.tab_feed, container, false);
+    songArrayList = new ArrayList<Song>();
 
-		getFacebookIdInBackground();
+    getFacebookIdInBackground();
 
-		com.beardedhen.androidbootstrap.BootstrapButton btn = (com.beardedhen.androidbootstrap.BootstrapButton) rootView
-				.findViewById(R.id.logout_btn);
-		btn.setOnClickListener(new View.OnClickListener() {
+    com.beardedhen.androidbootstrap.BootstrapButton btn = (com.beardedhen.androidbootstrap.BootstrapButton) rootView
+        .findViewById(R.id.logout_btn);
+    btn.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				ParseUser.getCurrentUser();
-				ParseUser.logOut();
-				Intent intent = new Intent(getActivity(), LoginActivity.class);
-				startActivity(intent);
+      @Override
+      public void onClick(View v) {
+        ParseUser.getCurrentUser();
+        ParseUser.logOut();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
 
-			}
-		});
+      }
+    });
 
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-		/* User List Friends */
+    RequestAsyncTask r = Request.executeMyFriendsRequestAsync(
+        ParseFacebookUtils.getSession(), new Request.GraphUserListCallback() {
 
-		Request.executeMyFriendsRequestAsync(ParseFacebookUtils.getSession(),
-				new Request.GraphUserListCallback() {
+          @SuppressWarnings("unchecked")
+          @Override
+          public void onCompleted(List<GraphUser> users, Response response) {
+            if (users != null) {
 
-					@SuppressWarnings("unchecked")
-					@Override
-					public void onCompleted(List<GraphUser> users,
-							Response response) {
-						if (users != null) {
-							List<String> friendsList = new ArrayList<String>();
-							for (GraphUser user : users) {
-								friendsList.add(user.getId());
-							}
-							songArrayList = new ArrayList<Song>();
-							// Construct a ParseUser query that will find
-							// friends whose
-							// facebook IDs are contained in the current user's
-							// friend list.
-							@SuppressWarnings("rawtypes")
-							ParseQuery friendQuery = ParseQuery.getUserQuery();
-							friendQuery.whereContainedIn("fbId", friendsList);
+              List<String> friendsList = new ArrayList<String>();
+              for (GraphUser user : users) {
+                friendsList.add(user.getId());
+              }
 
-							// findObjects will return a list of ParseUsers that
-							// are friends with
-							// the current user
-							try {
-								@SuppressWarnings("unused")
-								List<ParseObject> friendUsers = friendQuery
-										.find();
-								// Log.d("MyApp",
-								// "Size is=  " + friendUsers.size());
-								// Log.d("MyApp", "Name is=  "
-								// + friendUsers.get(0).get("fbId"));
-								// essentialy, friendUsers is a list of
-								// ParseUsers that are your facebook friends.
-								// Just get a list of all
-								// songs for each friendUser and display it in a
-								// listview.
+              @SuppressWarnings("rawtypes")
+              final ParseQuery friendQuery = ParseQuery.getUserQuery();
+              friendQuery.whereContainedIn("fbId", friendsList);
 
-								for (ParseObject obj : friendUsers) {
-									ParseUser currUser = (ParseUser) obj;
-									Log.d("User",
-											"name is " + currUser.get("fbId"));
+              friendQuery.findInBackground(new FindCallback<ParseObject>() {
 
-									try {
-										currUser.fetchIfNeeded();
-									} catch (ParseException e1) {
-										e1.printStackTrace();
-									}
+                @Override
+                public void done(List<ParseObject> friendUsers,
+                    ParseException e2) {
 
-									ParseQuery<ParseObject> query = ParseQuery
-											.getQuery("Song");
-									query.whereEqualTo("author", currUser);
+                  for (ParseObject obj : friendUsers) {
+                    // wtf
+                    ParseUser currUser = (ParseUser) obj;
+                    Log.d("User", "name is " + currUser.get("fbId"));
 
-									query.findInBackground(new FindCallback<ParseObject>() {
+                    try {
+                      currUser.fetchIfNeeded();
+                    } catch (ParseException e1) {
+                      e1.printStackTrace();
+                    }
 
-										@Override
-										public void done(
-												List<ParseObject> songListForSingleUser,
-												ParseException e) {
-											if (e == null && songListForSingleUser!=null) {
-												for (i = 0; i < songListForSingleUser.size(); i++) {
-													
-													songArrayList.add((Song) songListForSingleUser.get(i));
-											
-													if(songListForSingleUser != null) {
-													  Log.d("User" , "Size of the songlist is for curr user is =" + songListForSingleUser.size());
-													}
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery("Song");
+                    query.whereEqualTo("author", currUser);
 
+                    query.findInBackground(new FindCallback<ParseObject>() {
 
-												}
-											}
+                      @Override
+                      public void done(List<ParseObject> songListForSingleUser,
+                          ParseException e) {
+                        
+                        if (e == null && songListForSingleUser != null) {
 
-										}
+                          for (i = 0; i < songListForSingleUser.size(); i++) {
 
-									});
+                            songArrayList.add((Song) songListForSingleUser
+                                .get(i));
+                            Log.d("User", "Sizes are WHOLE + CURRENT "
+                                + songArrayList.size() + " "
+                                + songListForSingleUser.size());
 
-								}
-								
-								
-								//Log.d("User" , "Size of the songlist is =" + songArrayList.size());
+                          }
+                          //
+                        }
+                        try {
+                          Thread.sleep(4000);
+                        } catch (InterruptedException e2) {
+                          e2.printStackTrace();
+                        }
+                        
+                        Log.d("User", "Whole size is + " + songArrayList.size());
+                      } // here were done getting the songList for each
+                        // followed.
+                      
+                    });
+                    
+                  }
+                  
+                }
+              });
+           
+              
+            }
 
-							} catch (ParseException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				});
+          }
 
-		IntentFilter iF = new IntentFilter();
-		iF.addAction("com.android.music.metachanged");
-		// iF.addAction("com.android.music.playstatechanged");
-		// iF.addAction("com.android.music.playbackcomplete");
-		// iF.addAction("com.android.music.queuechanged");
-		// iF.addAction("com.android.music.musicservicecommand");
-		// iF.addAction("com.android.music.command");
-		getActivity().registerReceiver(mReceiver, iF);
-		return rootView;
-	}
+        });
+    
+   
+    
+    IntentFilter iF = new IntentFilter();
+    iF.addAction("com.android.music.metachanged");
+    getActivity().registerReceiver(mReceiver, iF);
+    return rootView;
+  }
 
-	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
+  private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
 
-			String action = intent.getAction();
-			String cmd = intent.getStringExtra("command");
-			Log.d("mIntentReceiver.onReceive", action + "/" + cmd);
+      String action = intent.getAction();
+      String cmd = intent.getStringExtra("command");
+      Log.d("mIntentReceiver.onReceive", action + "/" + cmd);
 
-			String artist = intent.getStringExtra("artist");
-			String album = intent.getStringExtra("album");
-			String track = intent.getStringExtra("track");
-			Log.d("Music", artist + ":" + album + ":" + track);
+      String artist = intent.getStringExtra("artist");
+      String album = intent.getStringExtra("album");
+      String track = intent.getStringExtra("track");
+      Log.d("Music", artist + ":" + album + ":" + track);
 
-			// everytime theres an update, push it to Parse track.
-			currSong = new Song();
-			currSong.setAuthor(ParseUser.getCurrentUser());
-			currSong.setTitle(track);
-			currSong.setArtist(artist);
-			currSong.saveInBackground();
+      // everytime theres an update, push it to Parse track.
+      currSong = new Song();
+      currSong.setAuthor(ParseUser.getCurrentUser());
+      currSong.setTitle(track);
+      currSong.setArtist(artist);
+      currSong.saveInBackground();
 
-		}
-	};
+    }
+  };
 
-	@SuppressWarnings("deprecation")
-	private static void getFacebookIdInBackground() {
-		Request.executeMeRequestAsync(ParseFacebookUtils.getSession(),
-				new Request.GraphUserCallback() {
-					@Override
-					public void onCompleted(GraphUser user, Response response) {
-						if (user != null) {
-							ParseUser.getCurrentUser()
-									.put("fbId", user.getId());
-							ParseUser.getCurrentUser().saveInBackground();
-							// use this part to get details from the logged in
-							// user
-						}
-					}
-				});
-	}
+  @SuppressWarnings("deprecation")
+  private static void getFacebookIdInBackground() {
+    Request.executeMeRequestAsync(ParseFacebookUtils.getSession(),
+        new Request.GraphUserCallback() {
+          @Override
+          public void onCompleted(GraphUser user, Response response) {
+            if (user != null) {
+              ParseUser.getCurrentUser().put("fbId", user.getId());
+              ParseUser.getCurrentUser().saveInBackground();
+              // use this part to get details from the logged in
+              // user
+            }
+          }
+        });
+  }
 
 }
