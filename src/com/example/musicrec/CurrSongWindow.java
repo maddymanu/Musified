fe2c3426d42fde5dfa70248aefa0322a7b67f30c
Aditya Bansal <aditya.bansal93@gmail.com>
2014-05-03 16:51:08 -0700

    Youtube button click works

--- src/com/example/musicrec/CurrSongWindow.java
+++ src/com/example/musicrec/CurrSongWindow.java
@@ -3,13 +3,12 @@ package com.example.musicrec;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.youtube.player.YouTubeIntents;
 
 public class CurrSongWindow extends Activity {
 
-
-  
   String artist, title;
 
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +17,18 @@ public class CurrSongWindow extends Activity {
 
     Bundle extras = getIntent().getExtras();
     if (extras != null) {
-       artist = extras.getString("ARTIST");
-       title = extras.getString("TITLE");
+      artist = extras.getString("ARTIST");
+      title = extras.getString("TITLE");
     }
 
-    Intent intent = YouTubeIntents.createSearchIntent(this, title + " by " + artist);
-    startActivity(intent);
+    
 
   }
 
-
-
-
-
-
-
-
+  public void openYoutubeSearch(View v) {
+    Intent intent = YouTubeIntents.createSearchIntent(this, title + " by "
+        + artist);
+    startActivity(intent);
+  }
 
 }
