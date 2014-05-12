18876c7a4a4fd61f789f0958dcf30a05eb229ef4
Aditya Bansal <aditya.bansal93@gmail.com>
2014-05-12 16:38:27 -0700

    Showing Song Image works!

--- src/com/example/musicrec/CurrSongWindow.java
+++ src/com/example/musicrec/CurrSongWindow.java
@@ -1,5 +1,8 @@
 package com.example.musicrec;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 import android.app.Activity;
@@ -9,18 +12,22 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.echonest.api.v4.Artist;
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
-import com.echonest.api.v4.Params;
 import com.echonest.api.v4.Song;
 import com.echonest.api.v4.SongParams;
 import com.google.android.youtube.player.YouTubeIntents;
@@ -37,6 +44,8 @@ public class CurrSongWindow extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.curr_song_window);
 
+    final ImageView songView = (ImageView) findViewById(R.id.songImage);
+
     /* TEMP SOL */
     StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
         .permitAll().build();
@@ -49,35 +58,95 @@ public class CurrSongWindow extends Activity {
       title = extras.getString("TITLE");
     }
 
-    // SongParams p = new Params();
-    // p.add("title", title);
-    // p.add("results", 10);
-    // p.add("artist", artist);
-    // p.includeTracks(); // the album art is in the track data
-    // p.setLimit(true); // only return songs that have track data
-    // p.addIDSpace("7digital-US");
-
     SongParams p = new SongParams();
     p.setArtist(artist);
     p.setTitle(title);
     p.includeTracks(); // the album art is in the track data
     p.setLimit(true); // only return songs that have track data
-    
     p.addIDSpace("7digital-US");
 
     try {
       List<Song> songs = en.searchSongs(p);
-      for (Song song : songs) {
+      Song song = songs.get(0);
         // get the release data from the first track returned for each song
-        String url = song.getString("tracks[0].release_image");
+        final String url = song.getString("tracks[0].release_image");
         Log.i("SONG INFO", song.getTitle() + " " + url);
 
-      }
+        // take the bitmap and calculate it just like CustomArrayAdapter in an
+        // AsyncTask
+
+        /* for SONG image */
+        AsyncTask<Void, Void, Bitmap> songImageTask = new AsyncTask<Void, Void, Bitmap>() {
+          protected Bitmap doInBackground(Void... p) {
+            Bitmap bm = null;
+            try {
+
+              // String url = currEchoArtistImage.getURL();
+              InputStream is = new URL(url).openStream();
+
+              BitmapFactory.Options options = new BitmapFactory.Options();
+              options.inJustDecodeBounds = true;
+
+              BitmapFactory.decodeStream(is, null, options);
+
+              options.inSampleSize = calculateInSampleSize(options, 200, 200);
+              options.inJustDecodeBounds = false;
+              is.close();
+
+              is = new URL(url).openStream();
+              bm = BitmapFactory.decodeStream(is, null, options);
+
+              int width = bm.getWidth();
+              int height = bm.getHeight();
+              int newWidth = 200;
+              int newHeight = 200;
+
+              float scaleWidth = ((float) newWidth) / width;
+              float scaleHeight = ((float) newHeight) / height;
+
+              Matrix matrix = new Matrix();
+              // resize the bit map
+              matrix.postScale(scaleWidth, scaleHeight);
+
+              bm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+            return bm;
+          }
+
+          protected void onPostExecute(Bitmap bm) {
+
+            /* SET THE SONG IMAAGE VIEW */
+            songView.setImageBitmap(bm);
+
+          }
+        };
+        songImageTask.execute();
+
+      
     } catch (EchoNestException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
+  }
+
+  private static int calculateInSampleSize(BitmapFactory.Options options,
+      int reqWidth, int reqHeight) {
+
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+      final int heightRatio = Math.round((float) height / (float) reqHeight);
+      final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+      inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+    }
+    return inSampleSize;
   }
 
   public void openYoutubeSearch(View v) {
