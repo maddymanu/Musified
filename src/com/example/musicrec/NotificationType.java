94e72ed81f4a2036df883624904fe86acd470a13
Aditya Bansal <maddymanu@hotmail.com>
2014-07-06 17:35:19 -0700

    Started Activity Feed/Notification Tray

---
+++ src/com/example/musicrec/NotificationType.java
@@ -0,0 +1,11 @@
+package com.example.musicrec;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import com.parse.ParseObject;
+
+@ParseClassName("NotificationType")
+public class NotificationType extends ParseObject {
+
+}
