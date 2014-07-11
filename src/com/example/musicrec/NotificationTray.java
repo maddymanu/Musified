173b680fd153f14ac143a82216ea9906c1941324
Aditya Bansal <maddymanu@hotmail.com>
2014-07-10 19:21:11 -0700

    Simple Noti List works with CardView.

--- src/com/example/musicrec/NotificationTray.java
+++ src/com/example/musicrec/NotificationTray.java
@@ -2,6 +2,7 @@ package com.example.musicrec;
 
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -19,15 +20,15 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 public class NotificationTray extends Activity {
-  
+
   ListView listview;
   NotificationsAdapter adapter;
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_notification_tray);
-    
-    TextView titleView = (TextView)findViewById(R.id.title);
+
+    TextView titleView = (TextView) findViewById(R.id.title);
     titleView.setText("Hey");
 
     // add listview images etc.
@@ -35,39 +36,42 @@ public class NotificationTray extends Activity {
 
     // only get new status notifications
     // TODO
-    
+
     ParseUser currUser = ParseUser.getCurrentUser();
-    ParseQuery<NotificationType> query = ParseQuery.getQuery("NotificationType");
+    ParseQuery<NotificationType> query = ParseQuery
+        .getQuery("NotificationType");
     query.whereEqualTo("fromUser", currUser);
     query.addDescendingOrder("createdAt");
     query.findInBackground(new FindCallback<NotificationType>() {
 
       @Override
-      public void done(List<NotificationType> notificationList, ParseException arg1) {
+      public void done(List<NotificationType> notificationList,
+          ParseException arg1) {
         listview = (ListView) findViewById(R.id.notificationListView);
-        Log.i("NOTI" , "size is= " + notificationList.size());
-        adapter = new NotificationsAdapter(NotificationTray.this.getApplicationContext(), 5 , notificationList);
+        Log.i("NOTI", "size is= " + notificationList.size());
+        adapter = new NotificationsAdapter(NotificationTray.this
+            .getApplicationContext(), 5, notificationList);
         listview.setAdapter(adapter);
       }
-      
+
     });
 
   }
 
 }
 
+@SuppressLint("NewApi")
 class NotificationsAdapter extends ArrayAdapter<NotificationType> {
-  
+
   public List<NotificationType> notificationsArrayList;
   private final Context c;
-  
 
-  
   public NotificationsAdapter(Context applicationContext, int i,
       List<NotificationType> notificationList) {
     // TODO Auto-generated constructor stub
-    super(applicationContext, i , notificationList);
+    super(applicationContext, i, notificationList);
     this.c = applicationContext;
+    this.notificationsArrayList = notificationList;
   }
 
   @Override
@@ -76,23 +80,32 @@ class NotificationsAdapter extends ArrayAdapter<NotificationType> {
         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
     // 2. Get rowView from inflater
-    View rowView = inflater.inflate(R.layout.single_notification_item, parent, false);
+    if (convertView == null) {
+      convertView = inflater.inflate(
+          R.layout.single_notification_item, null);
+    }
+
+    TextView titleView = (TextView) convertView.findViewById(R.id.title);
+    NotificationType currNotification = null;
+    try {
+      currNotification = (NotificationType) notificationsArrayList
+          .get(position);
+
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+    }
     
-    TextView titleView = (TextView) rowView.findViewById(R.id.title);
-    //NotificationType currNotification = (NotificationType) notificationsArrayList.get(position);
-    titleView.setText("jhBHDBjwedn2lel");
-    
-    
-    return rowView;
+    ParseUser fromUser = currNotification.getFromUser();
+    try {
+      fromUser.fetchIfNeeded();
+    } catch (ParseException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    titleView.setText(fromUser.get("name")+ " " + currNotification.getType() + "s your Song:");
+
+    return convertView;
   }
 
-
 }
-
-
-
-
-
-
-
-
