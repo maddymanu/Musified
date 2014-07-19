f600cdf1852130a96acd4d496c9e4aacf134c0da
Aditya Bansal <maddymanu@hotmail.com>
2014-07-18 17:10:05 -0700

    Shifted Inv friends to Sliding List View as well.

--- src/com/example/musicrec/Welcome.java
+++ src/com/example/musicrec/Welcome.java
@@ -1,9 +1,14 @@
 package com.example.musicrec;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,7 +20,18 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.FacebookError;
+import com.facebook.model.GraphUser;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 //Is basically the Tab Holder for different tabs
@@ -26,7 +42,9 @@ public class Welcome extends SherlockFragmentActivity {
   Fragment fragmentTab1 = new FragmentTab1();
   Fragment fragmentTab2 = new FragmentTab2();
   final Context context = this;
+  List<GraphUser> friendListForInvites = null;
 
+  @SuppressWarnings("deprecation")
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -36,8 +54,25 @@ public class Welcome extends SherlockFragmentActivity {
     View sliderView = inflater.inflate(R.layout.slider_list, null);
     ListView sliderList = (ListView) sliderView
         .findViewById(R.id.optionsListView);
-//    
-    String[] items2 = new String[] { "Notifications", "Settings", "Send", "Logout" , "Invite!"};
+
+    RequestAsyncTask r = Request.executeMyFriendsRequestAsync(
+        ParseFacebookUtils.getSession(), new Request.GraphUserListCallback() {
+
+          @SuppressWarnings("unchecked")
+          @Override
+          public void onCompleted(List<GraphUser> users, Response response) {
+            if (users != null) {
+
+              friendListForInvites = users;
+
+            }
+
+          }
+
+        });
+    //
+    String[] items2 = new String[] { "Notifications", "Settings", "Send",
+        "Logout", "Invite!" };
     ArrayAdapter<String> sliderListViewAdapter = new ArrayAdapter<String>(
         context, android.R.layout.simple_list_item_1, android.R.id.text1,
         items2);
@@ -67,19 +102,23 @@ public class Welcome extends SherlockFragmentActivity {
       @Override
       public void onItemClick(AdapterView<?> parent, View view, int position,
           long id) {
-        //Open Different Intents
-        
+        // Open Different Intents
+
         // Open Notification Menu
-        if(position == 0) { 
+        if (position == 0) {
           Intent intent = new Intent(Welcome.this, NotificationTray.class);
           startActivity(intent);
-        } 
+        }
         // Logout
-        else if(position == 3) {
+        else if (position == 3) {
           ParseUser.getCurrentUser();
           ParseUser.logOut();
           Intent intent = new Intent(Welcome.this, LoginActivity.class);
           startActivity(intent);
+        }
+        // Invite more friends
+        else if (position == 4) {
+          inviteFromFacebook(Welcome.this, friendListForInvites);
         }
       }
     });
@@ -105,5 +144,46 @@ public class Welcome extends SherlockFragmentActivity {
     inflater.inflate(R.menu.main, menu);
 
     return super.onCreateOptionsMenu(menu);
+  }
+
+  @SuppressWarnings("deprecation")
+  private void inviteFromFacebook(Activity activity, List<GraphUser> list) {
+
+    if (list == null || list.size() == 0)
+      return;
+
+    Bundle parameters = new Bundle();
+
+    parameters.putString("message", "Use my app!");
+
+    Facebook mFacebook = new Facebook("830750263621357");
+    // Show dialog for invitation
+    mFacebook.dialog(activity, "apprequests", parameters,
+        new Facebook.DialogListener() {
+          @Override
+          public void onComplete(Bundle values) {
+            // TODO Auto-generated method stub
+
+          }
+
+          @Override
+          public void onCancel() {
+            // TODO Auto-generated method stub
+
+          }
+
+          @Override
+          public void onFacebookError(FacebookError e) {
+            // TODO Auto-generated method stub
+
+          }
+
+          @Override
+          public void onError(DialogError e) {
+            // TODO Auto-generated method stub
+
+          }
+        });
+
   }
 }
